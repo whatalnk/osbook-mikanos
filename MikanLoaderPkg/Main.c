be1b0722309208f0c88cf8a3f1695dd8b004a1af
Wataru NAKAI <watnk0806@gmail.com>
2021-03-30 02:34:39 +0900

    day 03 a

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -6,6 +6,7 @@
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Uefi.h>
+#include <Guid/FileInfo.h>
 
 // @@range_begin(struct_memry_map)
 struct MemoryMap {
@@ -144,6 +145,51 @@ UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
   memmap_file->Close(memmap_file);
   // #@range_end(main)
 
+  // #@@range_begin(read_kernel)
+  EFI_FILE_PROTOCOL* kernel_file;
+  root_dir->Open(
+    root_dir, &kernel_file, L"\\kernel.elf", 
+    EFI_FILE_MODE_READ, 0
+  );
+
+  UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+  UINT8 file_info_buffer[file_info_size];
+  kernel_file->GetInfo(
+    kernel_file, &gEfiFileInfoGuid, 
+    &file_info_size, file_info_buffer
+  );
+
+  EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+  UINTN kernel_file_size = file_info->FileSize;
+
+  EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+  gBS->AllocatePages(
+    AllocateAddress, EfiLoaderData, 
+    (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+  kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+  Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+  // #@@range_end(read_kernel)
+
+  // #@@range_begin(exit_bs)
+  EFI_STATUS status;
+  status = gBS->ExitBootServices(image_handle, memmap.map_key);
+  if (EFI_ERROR(status)) {
+    status = GetMemoryMap(&memmap);
+    if (EFI_ERROR(status)){
+      Print(L"faild to get memory map: %r\n", status);
+      while (1);
+    }
+  }
+  // #@@range_end(exit_bs)
+
+  // #@@range_begin(call_kernel)
+  UINT64 entry_addr = *(UINT64*)(kernel_base_addr  + 24);
+
+  typedef void EntryPointType(void);
+  EntryPointType* entry_point = (EntryPointType*)entry_addr;
+  entry_point();
+  // #@@range_end(call_kernel)
+  
   Print(L"All done\n");
   while (1)
     ;
